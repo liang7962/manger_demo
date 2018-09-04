350988e5bcc032ac9dfe2e1f9338381f843eb99c
jinliang <123456>
2018-09-04 18:23:24 +0800

    bug修复

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @ServletComponentScan
 @SpringBootApplication
+@PropertySource({"classpath:application.properties"})
 @ImportResource({"classpath:/spring/applicationContext*.xml"})
 public class DemoApplication {
 
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,32 +1,16 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.LoanHoseDto;
 import com.example.demo.pojo.AdminRegister;
 import com.example.demo.service.TestService;
 import com.example.demo.util.LoggerUtils;
-import net.sf.json.JSONObject;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
-import org.apache.tomcat.util.http.fileupload.RequestContext;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
-import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.util.*;
 
 @Controller
 public class TestController {
@@ -52,7 +36,7 @@ public class TestController {
                                     @RequestParam(value="myfile4") MultipartFile file4,@RequestParam(value="name")String name
                                     ) throws IOException {
         LoggerUtils.info(getClass(),"name>>>>>>>>"+name);
-    //获取服务器中的真实file文件夹路径
+        //获取服务器中的真实file文件夹路径
         //生成真实的文件名称
         String truefile=file.getOriginalFilename();
         File mFile=new File("C:\\qt\\new");
