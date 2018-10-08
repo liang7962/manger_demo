2d49c00ee6ac926b472f65f8364d39ccb8e4a2e5
jinliang <123456>
2018-10-08 10:58:40 +0800

    定时任务

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ServletComponentScan
 @SpringBootApplication
 @PropertySource({"classpath:application.properties"})
 @ImportResource({"classpath:/spring/applicationContext*.xml"})
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,11 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.pojo.AdminRegister;
 import com.example.demo.service.TestService;
+import com.example.demo.util.LoggerUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Configurable
+@EnableScheduling
 public class TestController {
 
     @Autowired
@@ -19,6 +25,9 @@ public class TestController {
         return ar.getMobileNo()+"wewe";
     }
 
-
+    @Scheduled(cron = "0/5 * * * * *")
+    public void Task(){
+        LoggerUtils.info(getClass(),"定时任务来了>>>>>>>>>>>>>>>>>");
+    }
 
 }
